c9f8b1e947b14371e19b2b6e1890c19d3ce301cc
Leo Katzengruber <leofel.katz@gmail.com>
2023-08-03 20:13:36 +0200

    treesitter goodness

---
+++ lua/thekatze/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+return {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {
+        -- char = '│',
+        -- char = '╎',
+        -- char = '┆',
+        char = '┊',
+        show_trailing_blankline_indent = false,
+    },
+}
