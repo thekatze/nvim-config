50dfec7f3ff02ae309990fe5d761afc7e34f71b8
Leo Katzengruber <leofel.katz@gmail.com>
2024-01-03 14:43:07 +0100

    configure leap

--- lua/thekatze/plugins/leap.lua
+++ lua/thekatze/plugins/leap.lua
@@ -2,6 +2,16 @@ return {
     "ggandor/leap.nvim",
     event = "VeryLazy",
     config = function()
-        require('leap').add_default_mappings()
+        vim.keymap.set({ "v", "x" }, "s", function()
+            require('leap').leap { target_windows = { vim.api.nvim_get_current_win() } }
+        end)
+
+        vim.keymap.set("n", "s", function()
+            local focusable_windows = vim.tbl_filter(
+                function(win) return vim.api.nvim_win_get_config(win).focusable end,
+                vim.api.nvim_tabpage_list_wins(0)
+            )
+            require('leap').leap { target_windows = focusable_windows }
+        end)
     end,
 }
