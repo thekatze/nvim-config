ebffcd012fcf6071c5ce277873f830e01f970f48
Leo Katzengruber <leofel.katz@gmail.com>
2024-08-04 01:17:20 +0200

    remove zen mode

--- lua/thekatze/plugins/zen-mode.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/zen-mode.nvim",
-    event = "VeryLazy",
-    init = function()
-        local zenmode = require("zen-mode")
-        vim.keymap.set("n", "<leader>zz", zenmode.toggle, { desc = "Zen mode" })
-    end
-}
