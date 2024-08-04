7b6d0455ba2dbcf6aa983db05e66b6e990fc342e
Leo Katzengruber <leofel.katz@gmail.com>
2024-08-04 16:32:13 +0200

    fix toggleterm insert mode on open

--- lua/thekatze/plugins/toggleterm.lua
+++ lua/thekatze/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     event = "VeryLazy",
+    opts = {},
     config = function()
         local Terminal = require("toggleterm.terminal").Terminal
         local lazygit = Terminal:new({
@@ -16,10 +17,6 @@ return {
                 vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>",
                     { noremap = true, silent = true })
             end,
-            -- function to run on closing the terminal
-            on_close = function(term)
-                vim.cmd("startinsert!")
-            end,
         });
 
         vim.keymap.set("n", "<leader>g", function() lazygit:toggle() end, { desc = "lazygit" })
@@ -28,6 +25,9 @@ return {
             hidden = true,
             direction = "float",
             dir = "git_dir",
+            on_open = function()
+                vim.cmd("startinsert!")
+            end
         })
 
         vim.keymap.set({ "n", "t" }, "<C-t>", function() SimpleTerm:toggle() end, { desc = "Open terminal" })
